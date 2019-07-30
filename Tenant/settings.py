e40bf58901a4cf6b50c987ca82d366e6d889a986
njuguna <antony.njuguna@kaysalt.com>
2019-07-30 11:03:23 +0300

    Added database

--- Tenant/settings.py
+++ Tenant/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'Tenant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'ENGINE':'django.db.backends.mysql',
+         'NAME': 'Tenant',
+         'USER': 'root',
+         'PASSWORD': 'ksl@123',
+         'HOST': 'localhost',
+         'PORT':'' 
+        } 
 }
 
 
